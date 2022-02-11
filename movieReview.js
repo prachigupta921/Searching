498be2ff472ebb6abbd42b27d7113a53bf4d83dc
prachigupta921 <81792013+prachigupta921@users.noreply.github.com>
2022-02-11 09:51:01 +0530

    Add files via upload

---
+++ movieReview.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import PropTypes from "prop-types";
+
+const Moviereview = (props) => {
+  // console.log(props);
+  
+  return (
+    <div>
+      movieReview
+      <br />
+      {Image}
+    </div>
+  );
+};
+
+export default Moviereview;
